7794d4f4e4eafb3345460a215f697bb18474dc3f
Ed <edouard.liegeon@gmail.com>
2022-06-04 12:01:59 +0200

    Add display buttons if you aren't connected and begin CreateForm

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -3,8 +3,9 @@
 namespace App\Controller;
 use App\Entity\Tricks;
 use App\Repository\TricksRepository;
-use App\Entity\Comments;
 use App\Repository\CommentsRepository;
+use App\Repository\UsersRepository;
+use App\Form\CreateTrick;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,15 +20,20 @@ class TricksController extends AbstractController
         return $this->render('tricks/index.html.twig', 
             ['tricks' => $tricksRepository->findAll()]);
     }
-    #[Route('/{name}', name: 'single')]
-    public function single(Tricks $tricks, CommentsRepository $commentsRepository): Response
-    {
-        return $this->render('/tricks/single.html.twig', ['tricks'=> $tricks, 'comments'=> $commentsRepository->findBy(['tricks_Id' =>$tricks->id])]);
-    }
-    #[Route('/tricks/create', name:'create')]
+    #[Route('/create', name:'create')]
     public function create()
     {
-        return $this->render('/tricks/create.html.twig');
+        $form = $this->createForm(CreateTrickType::class, $tricks);
+        $form->handleRequest($request);
+
+
+        return $this->render('/tricks/create.html.twig', [ 'CreateTrick' => $form->createView()]);
     }
+    #[Route('/trick/{name}', name: 'single')]
+    public function single(Tricks $tricks, CommentsRepository $commentsRepository): Response
+    {
+         return $this->render('/tricks/single.html.twig', ['tricks'=> $tricks, 'comments'=> $commentsRepository->findBy(['tricks_Id' =>$tricks->id])]);
+    }
+    
     
 }
---
+++ src/Form/CreateTrickType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tricks;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateTrickType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('type')
+            ->add('photo')
+            ->add('video')
+            ->add('created_at')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tricks::class,
+        ]);
+    }
+}
