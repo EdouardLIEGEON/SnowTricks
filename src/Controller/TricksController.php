9d4cebfa825248ae6c0019be26d496f69748be90
Ed <edouard.liegeon@gmail.com>
2022-06-09 19:17:38 +0200

    add function to create a trick

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 use App\Entity\Tricks;
+use App\Form\CreateTrickType;
 use App\Repository\TricksRepository;
 use App\Repository\CommentsRepository;
 use App\Repository\UsersRepository;
-use App\Form\CreateTrick;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -20,15 +24,50 @@ class TricksController extends AbstractController
         return $this->render('tricks/index.html.twig', 
             ['tricks' => $tricksRepository->findAll()]);
     }
+    
     #[Route('/create', name:'create')]
-    public function create()
+    public function create(ManagerRegistry $doctrine, Request $request): Response
     {
+        $entityManager = $doctrine->getManager();
+        $tricks = new Tricks();
         $form = $this->createForm(CreateTrickType::class, $tricks);
         $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $tricks->setName($form->get('name')->getData())
+            ->setDescription($form->get('description')->getData())
+            ->setType($form->get('type')->getData())
+            ->setPhoto($form->get('photo')->getData())
+            ->setVideo($form->get('video')->getData());
+
+            $entityManager->persist($tricks);
+            $entityManager->flush();
+
+            return $this->redirect('/tricks');
+
+        }
 
         return $this->render('/tricks/create.html.twig', [ 'CreateTrick' => $form->createView()]);
     }
+
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete(Tricks $tricks = null, ManagerRegistry $doctrine): RedirectResponse
+    {
+        if($tricks){
+            $manager = $doctrine->getManager();
+            $manager->remove($tricks);
+            $manager->flush();
+            $this->addFlash(type:'success', message: 'Le trick est supprimé avec succès');
+
+        }else{
+
+            $this->addFlash(type:'success', message: 'Le trick est inexistant');
+
+        }
+        return $this->redirectToRoute(route:'/tricks');
+    }
+
     #[Route('/trick/{name}', name: 'single')]
     public function single(Tricks $tricks, CommentsRepository $commentsRepository): Response
     {
--- src/Form/CreateTrickType.php
+++ src/Form/CreateTrickType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Tricks;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +15,31 @@ class CreateTrickType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('type')
-            ->add('photo')
-            ->add('video')
-            ->add('created_at')
-        ;
+            ->add('name',  TextType::class , [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Nom'])
+            ->add('description', textType::class , [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Description'])
+            ->add('type', textType::class , [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Type de Trick'])
+            ->add('photo', FileType::class , [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Photo du Trick'])
+            ->add('video', TextType::class , [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Video du Trick']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
